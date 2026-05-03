d20b5e1f61760abbe9b4806aa81d16138d299b7a
Divyam Kumar Pandey <divyamkumarp@gmail.com>
2026-05-04 01:31:06 +0530

    Update dependencies and add security configuration; initialize database with patient data

---
+++ patientservice/src/main/java/com/pm/patientservice/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.pm.patientservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.disable())
+                );
+
+
+
+        return http.build();
+    }
+}
