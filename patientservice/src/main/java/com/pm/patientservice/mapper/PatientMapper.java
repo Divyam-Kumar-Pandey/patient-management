115a807002dbd286ec577d9014bc83f03cfc7a1c
Divyam Kumar Pandey <divyamkumarp@gmail.com>
2026-05-04 02:20:54 +0530

    Add PatientRequestDTO and implement createPatient method in PatientService; enhance PatientController with POST endpoint

--- patientservice/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patientservice/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -1,16 +1,34 @@
 package com.pm.patientservice.mapper;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
-        PatientResponseDTO patientResponseDTO = new PatientResponseDTO();
-        patientResponseDTO.setId(patient.getId().toString());
-        patientResponseDTO.setName(patient.getName());
-        patientResponseDTO.setEmail(patient.getEmail());
-        patientResponseDTO.setAddress(patient.getAddress());
-        patientResponseDTO.setDateOfBirth(patient.getDateOfBirth().toString());
-        return patientResponseDTO;
+        return new PatientResponseDTO(
+                patient.getId().toString(),
+                patient.getName(),
+                patient.getEmail(),
+                patient.getAddress(),
+                patient.getDateOfBirth().toString()
+        );
+
     }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        return new Patient(
+                null, // this will be automatically generated
+                patientRequestDTO.getName(),
+                patientRequestDTO.getEmail(),
+                patientRequestDTO.getAddress(),
+                LocalDate.parse(patientRequestDTO.getDateOfBirth()),
+                LocalDate.now()
+        );
+
+    }
+
 }
